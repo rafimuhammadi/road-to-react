5264c3f9e3568e1e60be9031c80c306cb25f21c5
rafimuhammadi <107848881+rafimuhammadi@users.noreply.github.com>
2022-10-13 12:51:26 +0430

    First Assignment Completion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+const todoLsit = [
+  {
+    id: 1,
+    title: "My First Assignment in CODE THE Dream.",
+  },
+  {
+    id: 2,
+    title: "I will be try to complete Assigment.",
+  },
+  {
+    id: 3,
+    title: "Finally I am completed my assignment.",
+  },
+  {
+    id: 4,
+    title: "LOVE YOU ALL CODE THE DREAM.",
+  },
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>My First Assigment on CODE THE DREAM</h1>
+      {/** Render the items */}
+      <ul>
+        {todoLsit.map((item) => {
+          return <li key={item.id}>{item.title}</li>;
+        })}
+      </ul>
+    </>
   );
 }
-
 export default App;
