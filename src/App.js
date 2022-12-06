402db7760aa7fdc9f0f677d49cf65312c3b830a8
rafimuhammadi <107848881+rafimuhammadi@users.noreply.github.com>
2022-12-06 21:24:30 +0430

    this for the adding into main brunc

--- src/App.js
+++ src/App.js
@@ -1,30 +1,6 @@
-<<<<<<< HEAD
-=======
-import React, { useState } from "react";
-import logo from "./logo.svg";
->>>>>>> a151ce1ba42344b9b467829180d27c4ac0c49f4d
 import "./App.css";
-import TodoList from "./TodoList";
-import AddTodoForm from "./AddTodoForm";
 import { Fragment } from "react";
-<<<<<<< HEAD
 function App() {
-  return (
-    <Fragment>
-      <h1>Todo List.</h1>
-      <AddTodoForm />
-      <TodoList />
-=======
-
-function App() {
-  const [newTodo, setNewTodo] = useState();
-  const id = Date.now();
-  return (
-    <Fragment>
-      <AddTodoForm onAddTodo={setNewTodo} />
-      <TodoList todoList={newTodo} id={id} />
->>>>>>> a151ce1ba42344b9b467829180d27c4ac0c49f4d
-    </Fragment>
-  );
+  return <h1>this is our main page</h1>;
 }
 export default App;
